2cb5f03827b5a26ea88e5135c99348c58401f948
aaroni34 <aaron.carricondo@gmail.com>
2023-05-03 10:13:43 +0200

    Add PodcastFilterer component

---
+++ src/components/PodcastFilterer.tsx
@@ -0,0 +1,32 @@
+import { Badge, Input, Row } from "antd";
+import { ChangeEvent } from "react";
+import { useAppSettings } from "../contexts/AppSettings";
+
+interface IPodcastFiltererProps {
+    badgeCount: number;
+    onSearchChange: (keyword: string) => void;
+}
+
+const PodcastFilterer: React.FC<IPodcastFiltererProps> = ({ badgeCount, onSearchChange}: IPodcastFiltererProps) => {
+    const { isLoading } = useAppSettings();
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { target } = e || {};
+        const { value } = target || {};
+        onSearchChange(value);
+      };
+
+    return (
+        <Row justify="end" align="middle">
+            <Badge color="blue" count={badgeCount} overflowCount={999}/>
+            <Input
+                placeholder="Filter podcasts..."
+                style={{ maxWidth: '400px', marginLeft: 16 }}
+                onChange={onChange}
+                disabled={isLoading}
+            />
+        </Row>
+    );
+};
+
+export default PodcastFilterer;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,10 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import IPodcast from "../models/IPodcast";
-import { Badge, Col, Input, Row, Spin } from "antd";
+import { Col, Row } from "antd";
 import PodcastCard from "../components/PodcastCard";
 import useTopPodcasts from "../hooks/useTopPodcasts";
 import { useAppSettings } from "../contexts/AppSettings";
+import PodcastFilterer from "../components/PodcastFilterer";
 
 const Main = () => {
   const { isLoading } = useAppSettings();
@@ -20,32 +21,20 @@ const Main = () => {
     return title.toLowerCase().includes(value) || author.toLowerCase().includes(value);
   }
 
-  const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { target } = e || {};
-    const { value } = target || {};
-
-    if (value) {
-      setFilteredPodcasts(topPodcasts.filter((p) => filterPodcast(p, value.toLowerCase())));
+  const onSearchChange = (keyword: string) => {
+    if (keyword) {
+      setFilteredPodcasts(topPodcasts.filter((p) => filterPodcast(p, keyword.toLowerCase())));
     }
     else setFilteredPodcasts(topPodcasts);
   };
 
   return (
     <>
-      {!isLoading && <div style={{ display: 'flex', justifyContent: 'end', alignItems: 'center', marginBottom: 16 }}>
-        <Badge color="blue" count={filteredPodcasts?.length} overflowCount={999} />
-        <Input
-          placeholder="Filter podcasts..."
-          style={{ maxWidth: '400px', marginLeft: 16 }}
-          onChange={onSearchChange}
-          disabled={isLoading}
-        />
-      </div>}
+      <PodcastFilterer badgeCount={filteredPodcasts?.length} onSearchChange={onSearchChange} />
       {!isLoading && <Row gutter={[32, 64]}>
         {filteredPodcasts?.map((podcast) => {
-          const { id } = podcast || {};
           return (
-            <Col key={id} span={6}>
+            <Col key={podcast.id} span={6}>
               <PodcastCard podcast={podcast} />
             </Col>
           );
