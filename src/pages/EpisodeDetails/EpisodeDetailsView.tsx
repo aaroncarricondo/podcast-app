32371a8abb72c87a66f36b1bd6094d50cc4e367f
Aaron Carricondo <39223492+aaroncarricondo@users.noreply.github.com>
2023-07-06 10:27:38 +0200

    Add skeleton to audio element

--- src/pages/EpisodeDetails/EpisodeDetailsView.tsx
+++ src/pages/EpisodeDetails/EpisodeDetailsView.tsx
@@ -1,4 +1,5 @@
-import { Card, Typography } from "antd";
+import { useState } from "react";
+import { Card, Skeleton, Typography } from "antd";
 import { useAppSettings } from "../../contexts/AppSettings";
 import IPodcastTrack from "../../models/IPodcastTrack";
 import '../../styles/episodeDetails.css';
@@ -11,13 +12,15 @@ interface IEpisodeDetailsViewProps {
 
 const EpisodeDetailsView: React.FC<IEpisodeDetailsViewProps> = ({ episode }: IEpisodeDetailsViewProps) => {
   const { isLoading } = useAppSettings();
+  const [loading, setLoading] = useState(true);
   const { trackName, description, episodeUrl } = episode || {};
 
   return (
     <Card loading={isLoading}>
       <Title level={2} className="episode-details-title">{trackName}</Title>
       <div className="episode-details-html-content" dangerouslySetInnerHTML={{ __html: description }} />
-      <audio className="episode-details-audio" controls src={episodeUrl} />
+      {loading && <Skeleton.Button className="episode-details-audio" active={true} size="large" shape="round" />}
+      <audio className="episode-details-audio" controls src={episodeUrl} onCanPlay={() => setLoading(false)} hidden={loading} />
     </Card>
   );
 };
